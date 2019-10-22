e9c0af90ffd9a180845ec6561bc1dbe0e9fb4a10
nmarth <nmarth2993@gmail.com>
2019-10-22 09:52:59 -0400

    Create SquareRootPrimeCounter.java

---
+++ SquareRootPrimeCounter.java
@@ -0,0 +1,34 @@
+
+public class SquareRootPrimeCounter {
+	
+	public boolean isPrime(int n) {
+		if (n <= 1) {
+			return false;
+		}
+		
+		for (int i = 2; i <= Math.sqrt(n); i++) {
+			if (n % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public int countPrimes(int limit) {
+		int primes = 0;
+		
+		for (int i = 0; i < limit; i++) {
+			if (isPrime(i)) {
+				System.out.println(i);
+				primes++;
+			}
+		}
+		return primes;
+	}
+	
+	public static void main(String[] args) {
+		SquareRootPrimeCounter p = new SquareRootPrimeCounter();
+		System.out.println(p.isPrime(47));
+//		System.out.println(p.countPrimes(100));
+	}
+}
