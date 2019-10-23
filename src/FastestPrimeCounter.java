b218b9477c188b820527a0ac0bb897fa7dc33dd2
nmarth2993 <nmarth2993@gmail.com>
2019-10-22 22:04:38 -0400

    Test

--- src/FastestPrimeCounter.java
+++ src/FastestPrimeCounter.java
@@ -3,33 +3,46 @@ public class FastestPrimeCounter {
 	private int numPrimes;
 	
 	public boolean isPrime(int n) {
+		if (n <= 1) {
+			return false;
+		}
 		for (int i = 0; i < alreadyFound.length && alreadyFound[i] > Math.sqrt(n); i++) {
-			if (n % alreadyFound.length == 0) {
+			if (n % alreadyFound[i] == 0) {
 				return false;
 			}
 		}
+		addPrime(n);
+		numPrimes++;
 		return true;
 	}
 	
 	public int countPrimes(int limit) {
+		int primeCount = 0;
 		alreadyFound = new int[100];
 		for (int i = 0; i < limit; i++) {
 			if (isPrime(i)) {
-				
+				primeCount++;
 			}
 		}
+		return primeCount;
 	}
 	
 	private void addPrime(int n) {
-		if (numPrimes > alreadyFound.length) {
+		if (numPrimes >= alreadyFound.length) {
 			int[] temp = new int[alreadyFound.length * 2];
 			for (int i = 0; i < alreadyFound.length; i++) {
 				temp[i] = alreadyFound[i];
 			}
+			temp[numPrimes] = n;
 			alreadyFound = temp;
+		}
+		else {
+			alreadyFound[numPrimes] = n;
 		}
 	}
 	
 	public static void main(String[] args) {
+		FastestPrimeCounter p = new FastestPrimeCounter();
+		System.out.println(p.countPrimes(100));
 	}
 }
